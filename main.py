acdec98e9ce68a261110804970a62e3ef59850df
dhrutube <dhruvsehgal2554@gmail.com>
2026-05-06 15:11:46 -0700

    main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 def main():
-    print("Hello from dsc-190-homework-5!")
-
+    hello = 'a string that uses single quotes'
+    if x == None:   # E711: use `is None` instead
+        pass
+    print( "Hello from dsc-190-homework-5!" )
 
 if __name__ == "__main__":
     main()
